81fa3c5fdb94bf88b78d0c73efbfe2ea16a31c8b
141-Dark <62534636+141-Dark@users.noreply.github.com>
2020-05-08 21:36:10 +0800

    1、增加了eureka子模块作为注册服务端 2、将单个的maven项目改造成了父子项目，其中父模块中的src文件删除 3、新增system子模块

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -2,8 +2,10 @@ package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer//声明这是服务器端的注册中心
 public class EurekaApplication {
 
     public static void main(String[] args) {
