a89cab941190379db2982fb1b1d89349b93c051a
141-Dark <62534636+141-Dark@users.noreply.github.com>
2020-05-11 18:34:39 +0800

    1、增加了eureka子模块作为注册服务端 2、将单个的maven项目改造成了父子项目，其中父模块中的src文件删除 3、新增system子模块 4、将system注册到eureka中 5、新增了一个gateway子模块，并添加到了注册中心 6、路由转发成功 7、集成了mybatis框架并启动成功 8、在springboot中搭建好了基本的框架 9、使用database关联数据库 10、新增数据库脚本 11、集成了热部署 12、优化了mybatis的输出信息，方便测试 13、新建公共模块server 14、將通用配置移到server中 15、集成了mybatis-generator 16、使用Example实现单表的操作

--- server/src/main/java/com/course/server/service/TestService.java
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -1,5 +1,6 @@
 package com.course.server.service;
 import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
 import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,10 @@ public class TestService {
     @Resource
     private TestMapper testMapper;
     public List<Test> list(){
-        return testMapper.list();
+        TestExample testExample = new TestExample();
+
+        //testExample.setOrderByClause("id desc");//根据id倒叙，作为条件
+        testExample.createCriteria().andIdEqualTo("2");
+        return testMapper.selectByExample(testExample);//根据条件输出
     }
 }
