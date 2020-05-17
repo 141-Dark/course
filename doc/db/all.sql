ee64fdfbb98d4d9b06fef71435561863ae291006
141-Dark <62534636+141-Dark@users.noreply.github.com>
2020-05-17 11:43:46 +0800

    1、增加大章表，并生成持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,1 +1,9 @@
+CREATE TABLE `course`.`Untitled`  (
+  `id` char(8) NOT NULL COMMENT 'ID',
+  `course_id` char(8) NULL COMMENT '课程id',
+  `name` varchar(50) NULL COMMENT '名称',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4;
+
+
 insert into `test`(id,name ) values(3,'蒸汽小飞机')
